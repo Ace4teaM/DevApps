51a0d738ac8a7c7bd96f80ab919d2446ec56e307
Thomas Auguey <thomas.auguey@hotmail.com>
2025-04-21 18:06:59 +0200

    Permet de créer des pointeurs d'objets

---
+++ DevApps/GUI/NewPointer.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DevApps.GUI
+{
+    /// <summary>
+    /// Logique d'interaction pour NewPointer.xaml
+    /// </summary>
+    public partial class NewPointer : Window, INotifyPropertyChanged
+    {
+        public string Value { get; set; }
+        public string ValidationMessage { get; set; }
+
+        private Regex Format = new Regex("^[A-z0-9_]+$");
+
+        public NewPointer()
+        {
+            InitializeComponent();
+            ValidationMessage = "Veuillez saisir un nom de pointeur";
+            this.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && String.IsNullOrEmpty(ValidationMessage))
+            {
+                this.DialogResult = true;
+                this.Close();
+            }
+
+            e.Handled = false;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(Value))
+                ValidationMessage = "Veuillez saisir un nom de pointeur";
+            else if (Format.IsMatch(Value) == false)
+                ValidationMessage = "Format invalide";
+            else
+                ValidationMessage = String.Empty;
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ValidationMessage"));
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            text.Focus();
+        }
+    }
+}
