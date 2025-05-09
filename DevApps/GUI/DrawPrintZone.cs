8ea3f685dbbc444c760a81868652903181243e70
Thomas Auguey <tauguey-aspi@groupesit.com>
2025-05-09 10:11:32 +0200

    Permet de zoomer sur la zone de dessin automatiquement à l'affichage de la facette

---
+++ DevApps/GUI/DrawPrintZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DevApps.GUI
+{
+    internal class DrawPrintZone : DrawBase
+    {
+        public DrawPrintZone()
+        {
+        }
+    }
+}
--- DevApps/LangageExtension.cs
+++ DevApps/LangageExtension.cs
@@ -1,4 +1,8 @@
-﻿public static class EnumerableExtensions
+﻿using System.Windows.Controls;
+using System.Windows;
+using DevApps.GUI;
+
+public static class EnumerableExtensions
 {
     public static bool ContainsAll<T>(this IEnumerable<T> source, IEnumerable<T> destination)
     {
@@ -7,5 +11,36 @@
 
         // Vérifie si tous les éléments de la séquence destination sont présents dans la séquence source
         return destination.All(item => source.Contains(item));
+    }
+}
+
+public static class CanvasExtensions
+{
+    public static Rect GetChildrenBoundingBox(this Canvas canvas)
+    {
+        if (canvas == null) throw new ArgumentNullException(nameof(canvas));
+
+        Rect boundingBox = Rect.Empty;
+
+        foreach (UIElement child in canvas.Children)
+        {
+            if (child is FrameworkElement fe)
+            {
+                double left = Canvas.GetLeft(fe);
+                double top = Canvas.GetTop(fe);
+
+                // Défaut à 0 si NaN
+                if (double.IsNaN(left)) left = 0;
+                if (double.IsNaN(top)) top = 0;
+
+                // Mesure si nécessaire (utile si layout non encore fait)
+                fe.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+
+                Rect childRect = new Rect(left, top, fe.DesiredSize.Width, fe.DesiredSize.Height);
+                boundingBox.Union(childRect);
+            }
+        }
+
+        return boundingBox;
     }
 }
