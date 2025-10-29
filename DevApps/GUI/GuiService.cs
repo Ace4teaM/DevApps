8ff38726d9558ca089e2c3787ce93af8a0c15a9a
Thomas Auguey <thomas.auguey@hotmail.com>
2025-10-29 10:58:25 +0100

    filtre les namespaces

--- DevApps/GUI/GuiService.cs
+++ DevApps/GUI/GuiService.cs
@@ -1,11 +1,8 @@
 ﻿using DevApps.Print;
 using Microsoft.Win32;
-using SharpVectors.Dom;
-using System;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
