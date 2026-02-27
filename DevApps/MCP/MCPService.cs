dc9c1dbbfb6a2cfb646d9dc9249d7dde3af6546d
Thomas Auguey <thomas.auguey@hotmail.com>
2026-02-27 14:32:35 +0100

    ajoute une implémentation minimaliste de MCP via stdio

---
+++ DevApps/MCP/MCPService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace DevApps.MCP
+{
+    /// <summary>
+    /// Serveur d'intéractions avec les modèles d'IA via le Model Context Protocol (MCP).
+    /// </summary>
+    internal static class MCPService
+    {
+        internal static Task? Current { get; private set; }
+        internal static IHost? CurrentHost { get; private set; }
+        internal static CancellationTokenSource Cancel { get; private set; } = new CancellationTokenSource();
+
+        internal static void Start()
+        {
+            var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+
+            builder.Services.AddMcpServer()
+                .WithStdioServerTransport() // utilise les flux d'entrée/sortie standard pour la communication
+                //.WithStreamServerTransport() // utilise un flux personnalisé (ex: TCP) pour la communication
+                .WithToolsFromAssembly(); // scan l'assembly pour trouver les outils à exposer [McpServerTool]
+
+            CurrentHost = builder.Build();
+
+            Current = CurrentHost.RunAsync(Cancel.Token);
+        }
+
+        internal static void Stop()
+        {
+            Cancel.Cancel();
+
+            // attendre la fin du service
+            CurrentHost?.StopAsync().Wait();
+        }
+    }
+}
---
+++ DevApps/MCP/ObjectsTools.cs
@@ -0,0 +1,26 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+using System.Globalization;
+using System.Net.Http;
+
+namespace DevApps.MCP
+{
+    [McpServerToolType]
+    public static class ObjectsTools
+    {
+        [McpServerTool, Description("Create a new object.")]
+        public static async Task<string> CreateObject(HttpClient client)
+        {
+            DevApps.Features.Objects.Create(out var name);
+            return name;
+        }
+
+        [McpServerTool, Description("Duplicate an existing object.")]
+        public static async Task<string> DuplicateObject(
+            HttpClient client,
+            [Description("Name of objet to duplicate")] string name)
+        {
+            return DevApps.Features.Objects.Duplicate(name);
+        }
+    }
+}
